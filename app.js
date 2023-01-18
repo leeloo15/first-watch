58ed3de7d8d73d4c3520db25530b4be2784b335b
Leeloo <leila150788@gmail.com>
2023-01-18 21:36:39 +0200

    setup

---
+++ app.js
@@ -0,0 +1,7 @@
+const date = new Date();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+const seconds = date.getSeconds();
+const year = date.getFullYear();
+const month = date.getMonth();
+const day = date.getDay();
--- src/pages/_app.jsx
+++
@@ -1,5 +0,0 @@
-import '../styles/index.scss';
-
-export default function App() {
-  return <div>app</div>;
-}
